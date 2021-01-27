638c9003333390d2e493ff0753229856fb2c6a86
Tara <TaraHussain97@gmail.com>
2021-01-27 12:19:23 +0000

    heroku with favicon

--- tests/test_website.py
+++ tests/test_website.py
@@ -2,10 +2,16 @@ import pytest
 import json
 
 
-def test_index_get(client):
+def test_index(client):
     res = client.get("/")
     assert res.status_code == 200
     assert "URL Shortener" in res.get_data(as_text=True)
     post_res = client.post("/")
     assert post_res.status_code != 200
     assert "http://127.0.0.1:5000/" in post_res.get_data(as_text=True)
+
+
+def test_display_short_url(client):
+    res = client.get("/display/test1")
+    assert res.status_code == 200
+    assert "http://127.0.0.1:5000/test1" in post_res.get_data(as_text=True)
